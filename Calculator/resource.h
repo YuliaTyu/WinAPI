7ba6c4b6b7221663706a35ca4eaecd2fa3f7b9e2
Юля <@DESKTOP-LDN2BMM>
2025-10-04 13:52:55 +0300

    Calculator Start

---
+++ Calculator/resource.h
@@ -0,0 +1,35 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Resource.rc
+//
+#define IDI_ICON1                          101
+#define IDI_BUTTON_0                       1000
+#define IDI_BUTTON_1                       1001
+#define IDI_BUTTON_2                       1002
+#define IDI_BUTTON_3                       1003
+#define IDI_BUTTON_4                       1004
+#define IDI_BUTTON_5                       1005
+#define IDI_BUTTON_6                       1006
+#define IDI_BUTTON_7                       1007
+#define IDI_BUTTON_8                       1008
+#define IDI_BUTTON_9                       1009
+#define IDI_BUTTON_POINT                   1010
+#define IDI_BUTTON_PLUS                    1011
+#define IDI_BUTTON_MINUS                   1012
+#define IDI_BUTTON_ASTER                   1013
+#define IDI_BUTTON_SLASH                   1014
+
+#define IDI_BUTTON_BSP                     1015
+#define IDI_BUTTON_CLR                     1016
+#define IDI_BUTTON_EQUAL                   1017
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
